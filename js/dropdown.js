37a1965b8eb805c3e45a5cdb4bdb735afb6e42ca
Andreas Sjölund <andreas.sjolund@chasacademy.se>
2017-09-26 13:42:59 +0200

    added sidenav markup and css

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,19 @@
-const subMenus = document.querySelectorAll('.list-menu--submenu');
+  // retrieve all ul's with a class that contains "sidenav__list-lv"
+  const subMenus = document.querySelectorAll('ul[class*="sidenav__list-lv"]');
 
-subMenus.forEach(function(subMenu) { 
-  
-  subMenu.previousElementSibling.addEventListener('click', function(e) {
-    e.preventDefault();
-    
-    subMenu.previousElementSibling.classList.toggle('list-menu-item--link__active');
-    
-    subMenu.style.maxHeight 
-    ? subMenu.style.maxHeight = null 
-    : subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-  });
-  
-})
+    // run a function for each submenu
+    subMenus.forEach(function(subMenu) {  
+
+      // add a listener for clicks to the previous sibling element
+      // in this case the a.sidenav__link
+      subMenu.previousElementSibling.addEventListener('click', function(e) {
+
+        // prevent browser default behaviour
+        e.preventDefault();
+
+        // toggle class to the submenu's parent which in this case is the li
+        subMenu.parentNode.classList.toggle('sidenav__item--is-active');
+        
+      });
+
+    });
