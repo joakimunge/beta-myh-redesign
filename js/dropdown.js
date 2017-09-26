b1e402cadb8215cbd39f9c9692e7b3968d90882a
Christoffer Malmgren <christoffer.malmgren@chasacademy.se>
2017-09-26 15:49:33 +0200

    new commit

---
+++ js/dropdown.js
@@ -0,0 +1,19 @@
+	// retrieve all ul's with a class that contains "sidenav__list-lv"
+	const subMenus = document.querySelectorAll('ul[class*="sidenav__list-lv"]');
+
+		// run a function for each submenu
+		subMenus.forEach(function(subMenu) { 	
+
+			// add a listener for clicks to the previous sibling element
+			// in this case the a.sidenav__link
+			subMenu.previousElementSibling.addEventListener('click', function(e) {
+
+				// prevent browser default behaviour
+				e.preventDefault();
+
+				// toggle class to the submenu's parent which in this case is the li
+				subMenu.parentNode.classList.toggle('sidenav__item--is-active');
+				
+			});
+
+		});
