f9208acc140034c1504e6c58fcc136eab0e53b50
lessp <ekander.tom@gmail.com>
2017-09-28 16:19:07 +0200

    Add sidenav scripts to JS-file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
 document.querySelector('.masthead__burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 });
+
+
+/*
+	Sidenav
+*/
+
+// retrieve all ul's with a class that contains "sidenav__list-lv"
+const subMenus = document.querySelectorAll('ul[class*="sidenav__list-lv"]');
+
+if (subMenus) {
+
+	// run a function for each submenu
+	subMenus.forEach(function(subMenu) { 	
+
+		// add a listener for clicks to the previous sibling element
+		// in this case the a.sidenav__link
+		subMenu.previousElementSibling.addEventListener('click', function(e) {
+
+			// prevent browser default behaviour
+			e.preventDefault();
+
+			// toggle class to the submenu's parent which in this case is the li
+			subMenu.parentNode.classList.toggle('sidenav__item--is-active');
+			
+		});
+
+	});
+
+}
